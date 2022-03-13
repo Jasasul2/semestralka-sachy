7bdafa590874041511f13fddc5b940f85db47b2e
JackPieCZ <72486584+JackPieCZ@users.noreply.github.com>
2022-03-13 16:14:51 +0100

    Fixed board showup

--- src/main/java/cz/cvut/fel/pjv/semestralka/ChessBoard.java
+++ src/main/java/cz/cvut/fel/pjv/semestralka/ChessBoard.java
@@ -5,31 +5,45 @@ package cz.cvut.fel.pjv.semestralka;
  * @author ondra
  */
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class ChessBoard extends JFrame {
+public class ChessBoard {
 
     public int windowSize, squaresCount, borderSize;
     public Color color1, color2;
 
     public ChessBoard(int windowSize, int squaresCount, int borderSize, ColorScheme colors) {
 
-        // Setting up the window 
-//        JFrame frame = new JFrame();
-//        frame.setTitle("Chess");
-//        frame.setSize(new Dimension (windowSize + 2 * borderSize, windowSize + 2 * borderSize));
-//        frame.setVisible(true);
-//        frame.getContentPane().setBackground(colors.background);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        setTitle("Chess");
-        setSize(windowSize + 2 * borderSize, windowSize + 2 * borderSize);
-        setVisible(true);
-        setBackground(colors.background);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Setting up the window
+        JFrame frame = new JFrame();
+        frame.setTitle("Chess");
+//        frame.setUndecorated(true); // vypne horni listu okna
+        frame.setLocationRelativeTo(null);
+//        frame.setLocationByPlatform(true);
+        frame.setBounds(borderSize, borderSize, windowSize+borderSize, windowSize+borderSize);
+        JPanel pn = new JPanel() {
+            @Override
+            public void paint(Graphics g){
+                for (int i = 0; i < squaresCount; i++) {
+                    for (int j = 0; j < squaresCount; j++) {
+                        if ((i+j)% 2 == 0){
+                            g.setColor(colors.player1);
+                        } else {
+                            g.setColor(colors.player2);
+                        }
+                        g.fillRect(i*64, j*64, 64, 64);
+                    }
+                    
+                }
+            }
+        };
+        pn.setBounds(0, 0, borderSize, borderSize);
+        frame.add(pn);
+        frame.setBackground(colors.background);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
         
         // Assigning the variables for later use
         this.windowSize = windowSize;
@@ -38,27 +52,4 @@ public class ChessBoard extends JFrame {
         this.color1 = colors.player1;
         this.color2 = colors.player2;
     }
-
-    @Override
-    public void paint(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-
-        // Drawing the chess pieces 
-        int colorIndex = 0;
-        Color[] colors = new Color[]{color1, color2};
-        int recSize = windowSize / squaresCount; 
-        
-        for (int y = 0; y < squaresCount; y++) {
-            for (int x = 0; x < squaresCount; x++) {
-                
-                g.setColor(colors[colorIndex]);
-                g.fillRect(borderSize + x * recSize, borderSize + y * recSize, recSize, recSize);
-                
-                colorIndex++;
-                colorIndex = colorIndex % 2;
-            }
-            colorIndex++;
-            colorIndex = colorIndex % 2;
-        }
-    }
-}
+}    
