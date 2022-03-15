f7c0e7f547cb14976466d20f774eaaa133c28883
JackPieCZ <72486584+JackPieCZ@users.noreply.github.com>
2022-03-15 19:39:58 +0100

    Optimized window size

--- src/main/java/cz/cvut/fel/pjv/semestralka/GameManager.java
+++ src/main/java/cz/cvut/fel/pjv/semestralka/GameManager.java
@@ -6,23 +6,24 @@ package cz.cvut.fel.pjv.semestralka;
  */
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class GameManager {
 
     // ----------------------------- ATTRIBUTES --------------------------------
-    public int windowSize;
     public int squaresCount;
     public Color color1;
     public Color color2;
     public Tile[][] chessboard;
     private final JFrame frame;
     private final JPanel masterPanel;
-    private JPanel board;
+    private final JPanel board;
 
     // ---------------------------- CONSTRUCTOR --------------------------------
     public GameManager(int tileRowCount, int tileSize, ColorScheme colors) {
@@ -31,19 +32,18 @@ public class GameManager {
         this.squaresCount = tileRowCount;
         this.color1 = colors.tileColor1;
         this.color2 = colors.tileColor2;
-        this.windowSize = tileRowCount * (tileSize + 2);
 
         // Setting up the window
         Tile.size = tileSize;
         frame = new JFrame();
         masterPanel = new JPanel();
-        masterPanel.setLayout(new GridLayout(0, 1, 50, 50));    
+        masterPanel.setLayout(new FlowLayout());
         masterPanel.setBorder(BorderFactory.createEmptyBorder(30,30,30,30));
         
         JPanel boardGrid = new JPanel();
-        boardGrid.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        boardGrid.setBorder(BorderFactory.createEmptyBorder());
         boardGrid.setLayout(new GridLayout(tileRowCount, tileRowCount));
-
+        
         // Creating the chessboard 
         chessboard = new Tile[tileRowCount][tileRowCount];
         boolean isColor1 = false;
@@ -54,19 +54,29 @@ public class GameManager {
                     isColor1 = !isColor1;
                     tileColor = color2;
                 }
-                chessboard[y][x] = new Tile(new Coordinates(x, y), isColor1, tileColor, board);
+                chessboard[y][x] = new Tile(new Coordinates(x, y), isColor1, tileColor, boardGrid);
             }
         }
         board = new JPanel();
         board.setLayout(new FlowLayout());
         board.add(boardGrid);
-        masterPanel.add(board, BorderLayout.CENTER);
         
+        // Creating board title
+        JLabel boardTitle = new JLabel();
+        boardTitle.setText("Chess Board");
+        
+        // Adding all widgets to main panel
+        masterPanel.add(boardTitle);//, BorderLayout.PAGE_START);
+        masterPanel.add(board);//, BorderLayout.PAGE_END);
+        
+        // Window settings
         frame.add(masterPanel);
         frame.setTitle("Chess");
         frame.setBackground(colors.background);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(600,650));
         frame.pack();
+        frame.setSize(new Dimension(600,650));
         frame.setVisible(true);
     }
     
